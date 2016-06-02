b0261b5e2105f54b4abd71ffb0034348004445e1
sakib717 <sakibsabbir7@gmail.com>
2016-06-01 22:59:04 -0400

    add AddActiveClass method to Site.Master.cs

--- COMP2007-Assignment1-SakibPatel/Site.Master.cs
+++ COMP2007-Assignment1-SakibPatel/Site.Master.cs
@@ -11,7 +11,46 @@ namespace COMP2007_Assignment1_SakibPatel
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            addActiveClass();
+        }
 
+        /**
+         * This method adds the active class to each li in the main
+         * navbar according to page title
+         * 
+         * 
+         * @method AddActiveClass
+         * @return {string}
+         * 
+         * 
+         */
+
+        private string addActiveClass()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+
+                case "Products":
+                    products.Attributes.Add("class", "active");
+                    break;
+
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+
+            return Page.Title;
         }
     }
 }
